5294cb7bc787ea73baa9e438f14bbd1810ab385a
aohno <naka2357@gmail.com>
2023-06-27 15:53:01 +0900

    mv template

--- study/views.py
+++ study/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def some_list(request):
     some_list: list[str] = ["abc", "def", "ghi"]
     context = {"some_list": some_list}
