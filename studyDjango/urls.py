385ffdb64d427774738244ae2f83e85d926e2689
aohno <naka2357@gmail.com>
2023-06-30 13:21:44 +0900

    blog markdown

--- studyDjango/urls.py
+++ studyDjango/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from study import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +27,4 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('mdeditor/', include('mdeditor.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
